56b8fec8f25894f81408d00fccecfb39634b51fb
Myungkyulee <skdlaudrb@naver.com>
2023-03-02 18:44:43 +0900

    feat : add API documentation by Swagger

--- src/main/java/com/example/weatherstudy/controller/DiaryController.java
+++ src/main/java/com/example/weatherstudy/controller/DiaryController.java
@@ -2,6 +2,8 @@ package com.example.weatherstudy.controller;
 
 import com.example.weatherstudy.domain.Diary;
 import com.example.weatherstudy.service.DiaryService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
@@ -16,26 +18,30 @@ public class DiaryController {
 
     // 날짜를 저장할 수 있는 방법은 많기 때문에 지정해 주어야 한다.
     // @DateTimeFormat은 날짜를 저장할 수 있는 포맷 중 하나를 설정해주는 애노테이션
+    @ApiOperation(value = "일기 텍스트와 날씨를 이용해서 DB에 일기 저장", notes = "이것은 노트")
     @PostMapping("/create/diary")
     public void createDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
                             @RequestBody String text) {
         diaryService.createDiary(date, text);
     }
 
+    @ApiOperation("선택한 날짜의 모든 일기 데이터를 가져옵니다.")
     @GetMapping("/read/diary")
     public List<Diary> readDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
         return diaryService.readDiary(date);
     }
 
+    @ApiOperation("선택한 기간 중의 모든 일기 데이터를 가져옵니다.")
     @GetMapping("/read/diaries")
-    public List<Diary> readDiaries(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
-                                   @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) {
+    public List<Diary> readDiaries(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @ApiParam(value = "조회할 기간의 마지막날", example = "2023-03-02") LocalDate startDate,
+                                   @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @ApiParam(value = "조회할 기간의 첫번째날", example = "2023-03-02") LocalDate endDate) {
         return diaryService.readDiaries(startDate, endDate);
     }
 
+    @ApiOperation("선택한 날짜의 일기의 텍스트를 수정합니다.")
     @PutMapping("/update/diary")
     public void updateDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
-                                LocalDate date,
+                            LocalDate date,
                             @RequestBody String text) {
         diaryService.updateDiary(date, text);
     }
